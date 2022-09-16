f174a5b91f5cc0dbf6abed3d71f6c7217d580fb6
Cornelio Posadas P <posadascornelio38@gmail.com>
2022-09-16 18:55:25 -0500

    update label for keyword

--- Codigo/funciones.py
+++ Codigo/funciones.py
@@ -5,3 +5,30 @@ def click(number):
 
 def clear():
     print('')
+
+def keyword(frame):
+
+
+    button_1 = Button(frame, text='1', padx = 40, pady= 20, command = lambda : click(1))
+    button_2 = Button(frame, text='2', padx = 40, pady= 20, command = lambda : click(2))
+    button_3 = Button(frame, text='3', padx = 40, pady= 20, command = lambda : click(3))
+    button_4 = Button(frame, text='4', padx = 40, pady= 20, command = lambda : click(4))
+    button_5 = Button(frame, text='5', padx = 40, pady= 20, command = lambda : click(5))
+    button_6 = Button(frame, text='6', padx = 40, pady= 20, command = lambda : click(6))
+    button_7 = Button(frame, text='7', padx = 40, pady= 20, command = lambda : click(7))
+    button_8 = Button(frame, text='8', padx = 40, pady= 20, command = lambda : click(8))
+    button_9 = Button(frame, text='9', padx = 40, pady= 20, command = lambda : click(9))
+    button_0 = Button(frame, text='0', padx = 40, pady= 20, command = lambda : click(0))
+    button_cl = Button(frame, text='Clear', padx = 79, pady= 20, command = lambda : clear())
+
+    button_1.grid(row = 3, column = 1)
+    button_2.grid(row = 3, column = 2)
+    button_3.grid(row = 3, column = 3)
+    button_4.grid(row = 2, column = 1)
+    button_5.grid(row = 2, column = 2)
+    button_6.grid(row = 2, column = 3)
+    button_7.grid(row = 1, column = 1)
+    button_8.grid(row = 1, column = 2)
+    button_9.grid(row = 1, column = 3)
+    button_0.grid(row = 4, column = 1)
+    button_cl.grid(row = 4, column= 2, columnspan=2)
--- Codigo/interfaz.py
+++ Codigo/interfaz.py
@@ -1,42 +1,25 @@
+from distutils.cmd import Command
 from tkinter import *
 from PIL import ImageTk, Image
 from funciones import *
 
+HEIGHT = 600
+WIDTH = 1200
+
+
 root = Tk()
 root.title('Interfaz Grafica')
 
-#led_e = ImageTk.PhotoImage(Image.open(r""))
-#label = Label(image = led_e)
 
-texto = Label(text = 'Teclado para display 7 segmentos')
+canvas = Canvas(root, height= HEIGHT, width=WIDTH)
+canvas.pack()
 
-button_1 = Button(root, text='1', padx = 40, pady= 20, command = lambda : click(1))
-button_2 = Button(root, text='2', padx = 40, pady= 20, command = lambda : click(2))
-button_3 = Button(root, text='3', padx = 40, pady= 20, command = lambda : click(3))
-button_4 = Button(root, text='4', padx = 40, pady= 20, command = lambda : click(4))
-button_5 = Button(root, text='5', padx = 40, pady= 20, command = lambda : click(5))
-button_6 = Button(root, text='6', padx = 40, pady= 20, command = lambda : click(6))
-button_7 = Button(root, text='7', padx = 40, pady= 20, command = lambda : click(7))
-button_8 = Button(root, text='8', padx = 40, pady= 20, command = lambda : click(8))
-button_9 = Button(root, text='9', padx = 40, pady= 20, command = lambda : click(9))
-button_0 = Button(root, text='0', padx = 40, pady= 20, command = lambda : click(0))
-button_cl = Button(root, text='Clear', padx = 79, pady= 20, command = lambda : clear())
+frame = Frame(root, bg = '#80c1ff')
+frame.place(relwidth=1, relheight=1)
 
-#texto = Text(root,text = 'Teclado para display 7 segmentos' )
-# but = Button(root, text='LED 1', padx = 79, pady= 20)
+frame_teclado = Frame(root, bg = '#80c1ff')
+frame_teclado.place(relwidth=0.25, relheight=0.45,x=850, y=250)
 
-button_1.grid(row = 3, column = 0)
-button_2.grid(row = 3, column = 1)
-button_3.grid(row = 3, column = 2)
-button_4.grid(row = 2, column = 0)
-button_5.grid(row = 2, column = 1)
-button_6.grid(row = 2, column = 2)
-button_7.grid(row = 1, column = 0)
-button_8.grid(row = 1, column = 1)
-button_9.grid(row = 1, column = 2)
-button_0.grid(row = 4, column = 0)
-button_cl.grid(row = 4, column= 1, columnspan=2)
-texto.grid(row = 0, column = 0, columnspan= 2)
-# label.grid(row = 1, column= 9)
+keyword(frame_teclado)
 
 root.mainloop()
