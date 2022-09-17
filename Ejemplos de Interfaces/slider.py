0edf6920d14b104188f88b43e550ab24a4b25cd4
Cornelio Posadas P <posadascornelio38@gmail.com>
2022-09-17 13:32:16 -0500

    stop working on sliders

--- Ejemplos de Interfaces/slider.py
+++ Ejemplos de Interfaces/slider.py
@@ -1,3 +1,16 @@
 from tkinter import *
 from PIL import ImageTk, Image
 
+
+root = Tk()
+root.title('Charts')
+root.geometry('400x400')
+
+vertical = Scale(root, from_ = 0, to = 200)
+vertical.pack()
+
+horizontal = Scale(root, from_=0, to=200, orient=HORIZONTAL)
+horizontal.pack()
+my_label = Label(root, text = horizontal.get()).pack()
+
+root.mainloop()
