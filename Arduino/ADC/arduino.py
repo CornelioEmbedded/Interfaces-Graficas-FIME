cd31e70982bae5d116f54d1511d0a2a88e93d981
Cornelio Posadas P <posadascornelio38@gmail.com>
2022-10-02 13:46:21 -0500

    adc worked!!

--- Arduino/ADC/arduino.py
+++ Arduino/ADC/arduino.py
@@ -1,20 +1,11 @@
-import serial
-from tkinter import *
+from funciones import MainFrame
+from tkinter import Tk
 
-display1 = serial.Serial('com3', 9600, timeout = 1)
+def main():
+    root = Tk()
+    root.wm_title("Sensores y Actuadores - Arduino")
+    app = MainFrame(root)
+    app.mainloop()
 
-def display_1():
-    display1.write(b'1')
-
-def display_0():
-    display1.write(b'0')
-
-root = Tk()
-
-button_1 = Button(root, text='1', command = display_1)
-button_1.grid(row = 0, column=0)
-
-button_0 = Button(root, text='0', command = display_0)
-button_0.grid(row = 1, column=1)
-
-root.mainloop()
+if __name__=="__main__":
+    main()
