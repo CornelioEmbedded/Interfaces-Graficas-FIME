62e81f7db7cb375c5de5121f8c069af50a6452cc
Cornelio Posadas P <posadascornelio38@gmail.com>
2022-10-02 22:21:48 -0500

    work on button python code

--- Arduino/BOTONES/BOTON/BOTON.ino
+++ Arduino/BOTONES/BOTON/BOTON.ino
@@ -5,33 +5,35 @@ int value_bot_2;
 int value_bot_3;
 int value_bot_4;
 
+int tiempo = 300;
+
 void get_bot_1()
 {
   value_bot_1 = digitalRead(9);
   Serial.println("bot[1]:" + String(value_bot_1));
 }
-Ticker tic_bot_1(get_bot_1, 500);
+Ticker tic_bot_1(get_bot_1, tiempo);
 
 void get_bot_2()
 {
   value_bot_2 = digitalRead(10);
   Serial.println("bot[2]:" + String(value_bot_2));
 }
-Ticker tic_bot_2(get_bot_2, 500);
+Ticker tic_bot_2(get_bot_2, tiempo);
 
 void get_bot_3()
 {
   value_bot_3 = digitalRead(11);
   Serial.println("bot[3]:" + String(value_bot_3));
 }
-Ticker tic_bot_3(get_bot_3, 500);
+Ticker tic_bot_3(get_bot_3, tiempo);
 
 void get_bot_4()
 {
   value_bot_4 = digitalRead(12);
   Serial.println("bot[4]:" + String(value_bot_4));
 }
-Ticker tic_bot_4(get_bot_4, 500);
+Ticker tic_bot_4(get_bot_4, tiempo);
 
 void setup() {
   Serial.begin(9600);
--- Arduino/BOTONES/funciones.py
+++ Arduino/BOTONES/funciones.py
@@ -9,28 +9,28 @@ class MainFrame(Frame):
         self.master = master                    
         self.pack()
         
-        self.hilo_pot1 = threading.Thread(target=self.getSensorValues,daemon=True)
-        self.hilo_pot2 = threading.Thread(target=self.getSensorValues,daemon=True)
-        self.hilo_pot3 = threading.Thread(target=self.getSensorValues,daemon=True)
-        self.hilo_pot4 = threading.Thread(target=self.getSensorValues,daemon=True)
+        self.hilo_bot1 = threading.Thread(target=self.getButtonValues,daemon=True)
+        self.hilo_bot2 = threading.Thread(target=self.getButtonValues,daemon=True)
+        self.hilo_bot3 = threading.Thread(target=self.getButtonValues,daemon=True)
+        self.hilo_bot4 = threading.Thread(target=self.getButtonValues,daemon=True)
 
         self.arduino = serial.Serial("COM3",9600)
         time.sleep(1)
         
-        self.value_pot1 = StringVar()
-        self.value_pot2 = StringVar()
-        self.value_pot3 = StringVar()
-        self.value_pot4 = StringVar()
+        self.value_bot1 = StringVar()
+        self.value_bot2 = StringVar()
+        self.value_bot3 = StringVar()
+        self.value_bot4 = StringVar()
 
         self.create_widgets()
         self.isRun=True
 
-        self.hilo_pot1.start()
-        self.hilo_pot2.start()
-        self.hilo_pot3.start()
-        self.hilo_pot4.start()
+        self.hilo_bot1.start()
+        self.hilo_bot2.start()
+        self.hilo_bot3.start()
+        self.hilo_bot4.start()
     
-    def getSensorValues(self):
+    def getButtonValues(self):
         while self.isRun:
             cad =self.arduino.readline().decode('ascii').strip()
             if cad:         
@@ -39,25 +39,25 @@ class MainFrame(Frame):
                 label=cad[:pos]
                 value=cad[pos+1:]                 
 
-                if label == 'pot[1]':
-                    self.value_pot1.set(value)
-                if label == 'pot[2]':
-                    self.value_pot2.set(value)
-                if label == 'pot[3]':
-                    self.value_pot3.set(value)
-                if label == 'pot[4]':
-                    self.value_pot4.set(value)
+                if label == 'bot[1]':
+                    self.value_bot1.set(value)
+                if label == 'bot[2]':
+                    self.value_bot2.set(value)
+                if label == 'bot[3]':
+                    self.value_bot3.set(value)
+                if label == 'bot[4]':
+                    self.value_bot4.set(value)
                 
     
     def create_widgets(self):
         Label(self,text="Pot[1]: ").place(x=30,y=20)
-        Label(self,width=6,textvariable=self.value_pot1).place(x=120,y=20)
+        Label(self,width=6,textvariable=self.value_bot1).place(x=120,y=20)
 
         Label(self,text="Pot[2]: ").place(x=30,y=50)
-        Label(self,width=6,textvariable=self.value_pot2).place(x=120,y=50)
+        Label(self,width=6,textvariable=self.value_bot2).place(x=120,y=50)
 
         Label(self,text="Pot[3]: ").place(x=30,y=80)
-        Label(self,width=6,textvariable=self.value_pot3).place(x=120,y=80)
+        Label(self,width=6,textvariable=self.value_bot3).place(x=120,y=80)
 
         Label(self,text="Pot[4]: ").place(x=30,y=110)
-        Label(self,width=6,textvariable=self.value_pot4).place(x=120,y=110)
+        Label(self,width=6,textvariable=self.value_bot4).place(x=120,y=110)
